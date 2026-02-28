cfa24d4555160e711440e9b355954a6b756a311c
Serina-6002 <yuchun.6002.seri@gmail.com>
2026-02-28 16:55:37 +0900

    Textareaコンポーネント作成 (#57) (#58)
    * Textareaコンポーネント作成
    
    * fix: improve accessibility for textarea
    
    * chore: run formatter
    
    * style: fix textarea css format

---
+++ src/component/Textarea/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./index.module.css";
+
+type Props = {
+  label?: string;
+  placeholder?: string;
+};
+
+export default function Textarea(props: Props) {
+  return (
+    <div className={styles.wrapper}>
+      {props.label && (
+        <label htmlFor="content" className={styles.label}>
+          {props.label}
+        </label>
+      )}
+
+      <textarea id="content" className={styles.textarea} placeholder={props.placeholder} />
+    </div>
+  );
+}
